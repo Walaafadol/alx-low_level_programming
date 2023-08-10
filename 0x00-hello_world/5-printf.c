2a6c2098a68e871e4920c984f759f9edc57c6cde
Walaafadol <walaafadul7@gmail.com>
2023-08-10 08:07:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main  -printf programmm
+* Return:0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+getchar();
+return (0);
+}
