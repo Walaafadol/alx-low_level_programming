52715e3238c80dcdbe92cf74afc95b6e4429e2a2
Walaafadol <walaafadul7@gmail.com>
2023-08-10 07:53:41 -0700

    return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main -aprogram that prints aline with puts
+ * Return:0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,")
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
