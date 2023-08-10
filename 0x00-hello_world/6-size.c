e60d81027031f760340b3587df94059b8bac83cf
Walaafadol <walaafadul7@gmail.com>
2023-08-10 12:20:57 -0700

    hh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char:%zu bytes\n", (unsigned long)sizeof(a));
-printf("Size of an int:%zu bytes\n", (unsigned long)sizeof(b));
-printf("Size of a long int:%zu bytes\n", (unsigned long)sizeof(c));
-printf("Size of a long long int:%zu bytes\n", (unsigned long)sizeof(d));
-printf("Size of a float: %zu bytes\n", (unsigned long)sizeof(e));
+printf("Size of a char:%zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int:%zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int:%zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
