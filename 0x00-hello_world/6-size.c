6ed32eb10a9f4d6b36f041420652f925f8feee6b
Walaafadol <walaafadul7@gmail.com>
2023-08-10 12:16:39 -0700

    hh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
-* main  -printf programmm
-* Return:0 (success
+* main  -printf programm
+* Return:0 (succes
 */
 int main(void)
 {
-char a;	 
+char a;
 int b;
 long int c;
 long long int d;
 float e;
-printf("Size of a char:%zu bytes\n", sizeof(a));
-printf("Size of an int:%zu bytes\n", sizeof(b));
-printf("Size of a long int:%zu bytes\n", sizeof(c));
-printf("Size of a long long int:%zu bytes\n", sizeof(d));
-printf("Size of a float: %zu bytes\n", sizeof(e));
+
+printf("Size of a char:%zu bytes\n", (unsigned long)sizeof(a));
+printf("Size of an int:%zu bytes\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%zu bytes\n", (unsigned long)sizeof(c));
+printf("Size of a long long int:%zu bytes\n", (unsigned long)sizeof(d));
+printf("Size of a float: %zu bytes\n", (unsigned long)sizeof(e));
 return (0);
 }
