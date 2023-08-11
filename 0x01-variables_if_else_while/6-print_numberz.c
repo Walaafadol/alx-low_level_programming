0840720a28e5d22dd5704ff47081300ab5dfcb0d
Walaafadol <walaafadul7@gmail.com>
2023-08-11 15:04:32 -0700

    ll

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+int u;
+for (u = '0'; u <= '9' ; u++)
+{
+putchar(u);
+}
+putchar('\n');
+return (0);
+}
