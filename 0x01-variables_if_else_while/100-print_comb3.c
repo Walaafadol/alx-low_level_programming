6d57a976a7de71e0155816e4d81f47e31bd497c4
Walaafadol <walaafadul7@gmail.com>
2023-08-11 16:13:15 -0700

    ll

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ for (a = '0'; a <= '9'; a++)
 {
 for (b = '0'; b <= '9'; b++)
 {
-if (a != b && b != a)
+if (a != b )
 {
-putchar(a);
-putchar(b);
+putchar(a + '0');
+putchar(b + '0');
 }
 if ((a != '9') || (a == '9' && b != '9'))
 {
