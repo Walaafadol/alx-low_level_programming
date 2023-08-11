0add33c120e52a25611583b7c16fe35b1f04beda
Walaafadol <walaafadul7@gmail.com>
2023-08-11 13:07:58 -0700

    uu

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z' ; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
