f5d9bb1905066343e31ae8e6fef17c6b37fa06ee
Walaafadol <walaafadul7@gmail.com>
2023-08-11 15:20:21 -0700

    hh

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+int h;
+for (h = '0'; h <= 'f'; h++)
+{
+putchar(h);
+}
+putchar('\n');
+return (0);
+}
