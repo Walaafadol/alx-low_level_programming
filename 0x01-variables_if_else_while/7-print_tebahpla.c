9e13004232fc71fa1208a84b11019b4c094f4050
Walaafadol <walaafadul7@gmail.com>
2023-08-11 15:12:24 -0700

    jj

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+char a;
+for (a = 'z'; a >= 'a'; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
