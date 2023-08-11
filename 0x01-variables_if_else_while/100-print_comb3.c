b85cb2a7a887e113d43ee770ace5f184b4ce91de
Walaafadol <walaafadul7@gmail.com>
2023-08-11 15:50:26 -0700

    kk

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a <= 9; a++)
+{
+putchar(a);
+for (b = 0; b <= 9; b++)
+{
+putchar(b);
+}
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
+
+
