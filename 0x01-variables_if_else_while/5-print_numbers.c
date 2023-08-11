337f63de4c794d3bcfbb86d6bf01ccd7ce8e0110
Walaafadol <walaafadul7@gmail.com>
2023-08-11 14:04:14 -0700

    jj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - aprogram that print results
+*Return:0 (sussess)
+*/
+int main(void)
+{
+int d;
+for (d = 0; d < 10; d++)
+{
+printf("%d\n", d);
+}
+return (0);
+}
+
