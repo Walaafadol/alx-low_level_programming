cbfbbdc0661447d05fdd17203e6cd6cb5bdb2025
Walaafadol <walaafadul7@gmail.com>
2023-08-11 09:08:51 -0700

    hh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d n is positive\n");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-ptintf("%d n is zero\n");
+ptintf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%d n is negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
