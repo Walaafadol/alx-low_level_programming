a426dccd0ec6941d9f754a5e3b4b7a3ea5eed0e0
Walaafadol <walaafadul7@gmail.com>
2023-08-15 10:54:15 -0700

    kk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ * function  print_alphabet print_alphabet
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
