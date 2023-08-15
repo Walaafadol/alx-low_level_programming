0ae915ddae765ae2fa7d6f8c706cb6013e511955
Walaafadol <walaafadul7@gmail.com>
2023-08-15 14:06:41 -0700

    ll

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ * Return:1 if c is lowercase
+ * @c:  is parameter
+ */
+int print_last_digit(int c)
+{
+int last;
+last = c % 10;
+return (last);
+}
