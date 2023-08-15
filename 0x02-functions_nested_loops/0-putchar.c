fedf0f04cf3ea4d9035fe28e1e8f60027c31f0b5
Walaafadol <walaafadul7@gmail.com>
2023-08-15 09:24:53 -0700

    ll

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char ch[50] = "_putchar";
+for (int i = 0; i <= 7; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n');
+}
+return (0);
