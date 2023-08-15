10993718df318bc2302c6299c7aa75e709c2845b
Walaafadol <walaafadul7@gmail.com>
2023-08-15 13:51:52 -0700

    ll

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - check the code.
+ * Return:1 if c is lowercase
+ * int _abs :  is parameter
+ */
+int _abs(int a)
+{
+if (a < 0)
+{
+_putchar(-a);
+}
+else
+{
+_putchar(a);
+}
