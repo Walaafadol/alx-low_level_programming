f8afcacedd80f18de07f05e3d59f6c9d1f363eed
Walaafadol <walaafadul7@gmail.com>
2023-08-15 14:31:28 -0700

    hh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,6 +12,8 @@ for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
 {
+if ((a <= 1 && b <= 9 ) || (a <= 2 && b <= 3))
+{
 for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
@@ -22,6 +24,7 @@ _putchar(58);
 _putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
+}
 }
 }
 }
