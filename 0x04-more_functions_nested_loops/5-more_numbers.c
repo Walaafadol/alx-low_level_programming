43fc6f9c1f28a3d8c1678ec699d5254f0794b416
Walaafadol <walaafadul7@gmail.com>
2023-08-17 15:10:11 -0700

    ll

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ * void
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int c, b;
+for (c = 0; c <= 9; c++)
+{
+for (b = 0; b <= 14; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
+
