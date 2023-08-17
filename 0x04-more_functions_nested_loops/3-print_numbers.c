857fe7a3f180f2b4fa40e5405bd3d45c5dda3ca7
Walaafadol <walaafadul7@gmail.com>
2023-08-17 13:33:22 -0700

    lkjh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * @c :parameter
+ * Return: Always 0.
+ */
+void print_numbers(int c)
+{
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+putchar('\n');
+}
+}
