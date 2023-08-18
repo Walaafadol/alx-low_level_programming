7e59b3bc85070e7971afd29d4d6806620628bdf4
Walaafadol <walaafadul7@gmail.com>
2023-08-17 17:26:13 -0700

    ll

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size:parameter
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int c, b, z;
+for ( c = 0; c < size; c++)
+{
+for (b = size - c; b > 1 ; b--)
+{
+_putchar('#');
+}
+for (z = 0; z <= c; z++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
