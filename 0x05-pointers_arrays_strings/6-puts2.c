25b2a676b0db9377e453a85831e3fece8f4efafb
Walaafadol <walaafadul7@gmail.com>
2023-08-21 09:58:49 -0700

    hh

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,12 @@
 void puts2(char *str)
 {
 int index;
-for (index = 0; str[index] != '\0'; index += 2)
+for (index = 0; str[index] != '\0'; index++)
+{
+if (str[index] % 2 == 0)
 {
 _putchar(str[index]);
+}
 }
 _putchar('\n');
 }
