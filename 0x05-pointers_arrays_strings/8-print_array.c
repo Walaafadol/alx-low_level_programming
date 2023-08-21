ff68ec93f07eb78eceeeca6d684b5fc8fee45580
Walaafadol <walaafadul7@gmail.com>
2023-08-21 11:41:27 -0700

    kk

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* reset_to_98 - check the code
+*print_array - check the code
 *@a: pointer
 *@n: pointer
 * Return: Always 0.
@@ -12,9 +12,9 @@ int i;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i != n-1)
+if (i != (n - 1))
 {
-printf(",");
+printf(", ");
 }
 }
 printf("\n");
