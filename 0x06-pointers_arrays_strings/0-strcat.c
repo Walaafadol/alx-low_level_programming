697005bc0c302c42e4ef450a13f3f5c167a30955
Walaafadol <walaafadul7@gmail.com>
2023-08-23 06:48:31 -0700

    kk

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 for (i = 0; dest[i] != '\0'; i++)
-{
-_putchar(dest[i]);
-}
+;
 for (j = 0; src[j] != '\0'; j++)
 {
-_putchar(src[j]);
+dest[i] = src[j];
+i++;
 }
+dest[i] = '\0';
 }
