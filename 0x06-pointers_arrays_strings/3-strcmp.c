565b1de446abb37058550de2ee54e6bb35811e00
Walaafadol <walaafadul7@gmail.com>
2023-08-23 10:28:59 -0700

    kk

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strcmp - check the code
+*@s1:parameter
+*@s2:parameter
+* Return: Always 0.
+*/
+int _strcmp(char *s1, char *s2)
+{
+int equal = 0;
+while (*s1)
+{
+if (*s1 != *s2)
+{
+equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+break;
+}
+s1++;
+s2++;
+}
+return (equal);
+}
