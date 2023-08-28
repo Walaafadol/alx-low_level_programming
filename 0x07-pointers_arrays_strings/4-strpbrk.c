112187e3a7130ad88c27f4c8b4b65d7cd9a79c01
Walaafadol <walaafadul7@gmail.com>
2023-08-28 13:40:05 -0700

    ggg

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strpbrk - check the code
+*@s:parameter
+*@accept:parameter
+* Return: Always 0.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
