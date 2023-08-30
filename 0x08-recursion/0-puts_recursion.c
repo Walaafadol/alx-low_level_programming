630231c9438f882a265462278d7b3bfeb779370a
Walaafadol <walaafadul7@gmail.com>
2023-08-30 07:31:44 -0700

    ll

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
 */
 void _puts_recursion(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
+if (*s == 0)
 {
-_putchar(s[i]);
-_putchar("\n");
-}
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
