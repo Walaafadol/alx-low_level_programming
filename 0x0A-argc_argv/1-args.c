0c09585f24e5e26ac9a77bf85855998fdf87a877
Walaafadol <walaafadul7@gmail.com>
2023-09-01 12:43:51 -0700

    kk

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - program
+*Return:0
+*@argc:parameter
+*/
+int main(int argc, char *argv[])
+{
+if (argc  && argv[0])
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
