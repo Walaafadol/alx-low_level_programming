a6d7e27327d1b6463fd76855a787d14220599323
Walaafadol <walaafadul7@gmail.com>
2023-09-11 13:49:11 -0700

    ll

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_H
+#define dog_H
+/**
+* struct dog - adog struct
+*@name:parameter
+*@age:parameter
+*@owner:owner dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
