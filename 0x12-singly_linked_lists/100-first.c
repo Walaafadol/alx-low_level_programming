def667e688faf8c9e131918336c03ddb32925511
Walaafadol <walaafadul7@gmail.com>
2023-09-21 14:05:28 -0700

    ll

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void _constructor(void) __attribute__ ((constructor));
+/**
+*_constructor - program
+*Return:void
+*/
+void _constructor(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
