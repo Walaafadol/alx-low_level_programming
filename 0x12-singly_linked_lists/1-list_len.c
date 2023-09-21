95d560d62b9d1e6143d87d7ec57d423c23908172
Walaafadol <walaafadul7@gmail.com>
2023-09-21 09:25:44 -0700

    kk

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+*list_len( - program
+*Return:0
+*@h:parameter
+*/
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+h =  h->next;
+i++;
+}
+return (i);
+}
