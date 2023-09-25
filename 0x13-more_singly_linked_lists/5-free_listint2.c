d551a2d2d90cae3ea654df840f6646edccc837ea
Walaafadol <walaafadul7@gmail.com>
2023-09-25 14:59:43 -0700

    gg

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+*free_listint2 - program
+*Return:0
+*@head:parameter
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *node, *temp;
+while (!*head)
+return;
+node = *head;
+while (node)
+{
+temp = node;
+node = node->next;
+free(temp);
+}
+*head = NULL;
+}
