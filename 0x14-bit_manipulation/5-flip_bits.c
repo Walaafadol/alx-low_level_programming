c99c9dbb2e42120452dbe2b8c9401582949f636b
Walaafadol <walaafadul7@gmail.com>
2023-09-28 14:57:53 -0700

    ll

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*flip_bits - program
+*Return:0
+*@m:parameter
+*@n:argument
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int xor = n ^ m;
+unsigned int count = 0;
+while (xor)
+{
+if (xor & 1lu)
+count++;
+xor = xor >> 1;
+}
+return (count);
+}
