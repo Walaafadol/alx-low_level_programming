ac33b29f9d335bb56521f420b5f7507dd5165391
Walaafadol <walaafadul7@gmail.com>
2023-09-28 14:51:12 -0700

    oo

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*clear_bit - program
+*Return:0
+*@index:argument
+*@n:argument
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+return (-1);
+if (*n & 1l << index)
+*n ^= 1l << index;
+return (1);
+}
