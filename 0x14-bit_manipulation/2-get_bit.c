7c79f1bdaf83fc9624037c05c46ff759df822c13
Walaafadol <walaafadul7@gmail.com>
2023-09-28 10:01:38 -0700

    hhh

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*get_bit - program
+*Return:0
+*@index:argument
+*@n:argument
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bit = sizeof(n) * 8;
+if (index >= bit)
+return (1);
+return (n >> index & 1);
+}
