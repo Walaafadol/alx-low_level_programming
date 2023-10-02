c4320d35580137303a1b6f563849b7310edf05ea
Walaafadol <walaafadul7@gmail.com>
2023-10-02 12:04:40 -0700

    ll

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*_strlen - program
+*Return:0
+*@s:parameter
+*/
+int _strlen(char *s)
+{
+int i = 0;
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
+
+#include "main.h"
+/**
+*create_file - program
+*Return:0
+*@text_content:argument
+*@filename:argument
+*/
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t bytes = 0, len = _strlen(text_content);
+if (!filename)
+return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (len)
+bytes = write(fd, text_content, len);
+close(fd);
+return (bytes = len ? 1 : -1);
+}
+
