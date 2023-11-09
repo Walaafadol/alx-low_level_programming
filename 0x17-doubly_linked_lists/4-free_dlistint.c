d27161dd8a26ca6dae0a7ec6250d4ee340cd71e1
Walaafadol <walaafadul7@gmail.com>
2023-11-09 12:20:41 -0800

    dddd

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+*free_dlistint - program
+*Return:0
+*@head:parameter
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *node;
+while (head)
+{
+node = head;
+head = head->next;
+free(node);
+}
+}
