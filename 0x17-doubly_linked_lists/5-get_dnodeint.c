79b936a76ca647a4829d1398f9b621e8cea9a7c9
Walaafadol <walaafadul7@gmail.com>
2023-11-09 12:34:42 -0800

    gggg

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*get_dnodeint_at_index - program
+*Return:0
+*@head:parameter
+*@index:argument
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+while (head)
+{
+if (i == index)
+return (head);
+head = head->next;
+i++
+}
+return (NULL);
+}
