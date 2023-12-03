69742c01c30a56914791602cfb7e380c6e8b62b8
Walaafadol <walaafadul7@gmail.com>
2023-12-03 13:38:29 -0800

    ll

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,5 +43,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 hash_node_t *make_hashnode(const char *key, const char *value);
-
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+shash_node_t *make_shash_node(const char *key, const char *value)
 #endif
